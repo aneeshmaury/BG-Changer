80dcdbd662bc11ef1272d66f92ab8d3201cbee56
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2023-12-30 18:17:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let bodyColor = document.querySelector('body');
+let redBtn = document.querySelector('.redbtn');
+let greenBtn = document.querySelector('.greenbtn');
+let yellowBtn = document.querySelector('.yellowbtn');  
+let blueBtn = document.querySelector('.bluebtn');
+let blackBtn = document.querySelector('.blackbtn');
+let orangeBtn = document.querySelector('.orangebtn');
+
+document.addEventListener("DOMContentLoaded", function () {
+yellowBtn.addEventListener("click", function() {
+    bodyColor.style.backgroundColor = "yellow";
+});
+
+})

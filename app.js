dcbdd90e0c1e7ffd2ed124ab075de1f3f5d172a7
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2023-12-31 07:43:32 +0530

    bg changer last commit

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-let bodyColor = document.querySelector('body');
-let redBtn = document.querySelector('.redbtn');
-let greenBtn = document.querySelector('.greenbtn');
-let yellowBtn = document.querySelector('.yellowbtn');  
-let blueBtn = document.querySelector('.bluebtn');
-let blackBtn = document.querySelector('.blackbtn');
-let orangeBtn = document.querySelector('.orangebtn');
 
-document.addEventListener("DOMContentLoaded", function () {
-yellowBtn.addEventListener("click", function() {
-    bodyColor.style.backgroundColor = "yellow";
-});
+        document.addEventListener("DOMContentLoaded", function () {
+            let bodyColor = document.querySelector('body');
+            let blueBtn = document.querySelector('.bluebtn');
+            let yellowBtn = document.querySelector('.yellowbtn');
+            let redBtn = document.querySelector('.redbtn');
+            let greenBtn = document.querySelector('.greenbtn');
+            let blackBtn = document.querySelector('.blackbtn');
+            let orangeBtn = document.querySelector('.orangebtn');
 
-})
+            blueBtn.addEventListener("click", function () {
+                bodyColor.style.backgroundColor = "blue";
+            });
+
+            yellowBtn.addEventListener("click", function () {
+                bodyColor.style.backgroundColor = "yellow";
+            });
+
+            redBtn.addEventListener("click", function () {
+                bodyColor.style.backgroundColor = "red";
+            });
+
+            greenBtn.addEventListener("click", function () {
+                bodyColor.style.backgroundColor = "green";
+            });
+
+            blackBtn.addEventListener("click", function () {
+                bodyColor.style.backgroundColor = "black";
+            });
+
+            orangeBtn.addEventListener("click", function () {
+                bodyColor.style.backgroundColor = "orange";
+            });
+        });
+    
